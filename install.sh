6382c16592c72784ef8b4f6ac6e8f9af1e1c0369
fredlblack2009 <fredlblack2009@gmail.com>
2016-03-08 15:24:22 +0800

    160308-

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "--------------------------------------------------------------------------------------"
-webaddr=
-hname=
-echo "$webaddr web2 start ..."
-scp /usr/local/jdk-8u65-linux-x64.gz root@${webaddr}:/usr/local/
-scp /usr/local/redis-3.0.4.tar.gz root@${webaddr}:/usr/local/
+webaddr=192.168.20.104
+hname=fhc-web1
+echo "$webaddr web1 start ..."
+scp /root/jdk-8u65-linux-x64.gz root@${webaddr}:/usr/local/
+scp /root/redis-3.0.4.tar.gz root@${webaddr}:/usr/local/
 ssh root@$webaddr << eeooff
 hostnamectl set-hostname $hname
-echo 'zxcv4321$#@!' | passwd --stdin root
+yum -y install gcc-c++
+yum -y install telnet
 useradd oracle
 echo 'qwer1234!@#$' | passwd --stdin oracle
 su - oracle
@@ -25,23 +26,23 @@ cd /usr/local/redis-3.0.4/deps
 make hiredis jemalloc linenoise lua
 cd /usr/local/redis-3.0.4
 make && make install
-cp redis.conf /etc/redis.conf
 exit
 eeooff
-scp oracle@192.168.21.209:/home/oracle/ct-data/ct-data-3.1.0.release.jar root@${webaddr}:/home/oracle/ct-data/
-scp oracle@192.168.21.209:/home/oracle/ct-data/startup.sh root@${webaddr}:/home/oracle/ct-data/
-scp oracle@192.168.21.209:/home/oracle/ct-data/config/application-product.properties root@${webaddr}:/home/oracle/ct-data/config/
-scp oracle@192.168.21.209:/home/oracle/ct-data/17monipdb.dat root@${webaddr}:/home/oracle/ct-data/
+scp /root/ct-data/ct-data-3.1.0.RELEASE.jar root@${webaddr}:/home/oracle/ct-data/
+scp /root/ct-data/startup.sh root@${webaddr}:/home/oracle/ct-data/
+scp /root/ct-data/config/application-product.properties root@${webaddr}:/home/oracle/ct-data/config/
+scp /root/ct-data/17monipdb.dat root@${webaddr}:/home/oracle/ct-data/
+scp /root/redis.conf root@${webaddr}:/etc/
+chown -R oracle:oracle /home/oracle
 echo "--------------------------------------------------------------------------------------"
 
 echo "--------------------------------------------------------------------------------------"
-webaddr=
-hname=
-echo "$webaddr payment start ..."
-scp /usr/local/jdk-8u65-linux-x64.gz root@${webaddr}:/usr/local/
-ssh root@$webaddr << eeooff
+hname=fhc-pay
+echo "localhost payment start ..."
+cp /root/jdk-8u65-linux-x64.gz /usr/local/
 hostnamectl set-hostname $hname
-echo 'zxcv4321$#@!' | passwd --stdin root
+yum -y install gcc-c++
+yum -y install telnet
 useradd oracle
 echo 'qwer1234!@#$' | passwd --stdin oracle
 su - oracle
@@ -53,31 +54,30 @@ cd /usr/local
 tar -zxvf jdk-8u65-linux-x64.gz
 alternatives --install /usr/sbin/java java /usr/local/jdk1.8.0_65/bin/java 1
 alternatives --install /usr/sbin/jps jps /usr/local/jdk1.8.0_65/bin/jps 1
-exit
-eeooff
-scp /home/oracle/ct-data-payment/ct-data-payment-3.1.0.release.jar root@${webaddr}:/home/oracle/ct-data-payment/
-scp /home/oracle/ct-data-payment/startup.sh root@${webaddr}:/home/oracle/ct-data-payment/
-scp /home/oracle/ct-data-payment/config/application-product.properties root@${webaddr}:/home/oracle/ct-data-payment/config/
-scp /home/oracle/ct-data-baccarat/ct-data-baccarat-3.1.0.RELEASE.jar root@${webaddr}:/home/oracle/ct-data-baccarat/
-scp /home/oracle/ct-data-baccarat/startup.sh root@${webaddr}:/home/oracle/ct-data-baccarat/
-scp /home/oracle/ct-data-baccarat/config/application-product.properties root@${webaddr}:/home/oracle/ct-data-baccarat/config/
+cp /root/ct-data-payment/ct-data-payment-3.1.0.RELEASE.jar /home/oracle/ct-data-payment/
+cp /root/ct-data-payment/startup.sh /home/oracle/ct-data-payment/
+cp /root/ct-data-payment/config/application-product.properties /home/oracle/ct-data-payment/config/
+cp /root/ct-data-baccarat/ct-data-baccarat-3.1.0.RELEASE.jar /home/oracle/ct-data-baccarat/
+cp /root/ct-data-baccarat/startup.sh /home/oracle/ct-data-baccarat/
+cp /root/ct-data-baccarat/config/application-product.properties /home/oracle/ct-data-baccarat/config/
+chown -R oracle:oracle /home/oracle
 echo "--------------------------------------------------------------------------------------"
 
 echo "--------------------------------------------------------------------------------------"
-webaddr=
-hname=
-echo "$webaddr web1 start ..."
-scp /usr/local/jdk-8u65-linux-x64.gz root@${webaddr}:/usr/local/
-scp /usr/local/nginx-1.9.5.tar.gz root@${webaddr}:/usr/local/
+webaddr=192.168.20.105
+hname=fhc-web2
+echo "$webaddr web2 start ..."
+scp /root/jdk-8u65-linux-x64.gz root@${webaddr}:/usr/local/
+scp /root/nginx-1.9.5.tar.gz root@${webaddr}:/usr/local/
 ssh root@$webaddr << eeooff
 hostnamectl set-hostname $hname
-echo 'zxcv4321$#@!' | passwd --stdin root
+yum -y install gcc-c++
+yum -y install telnet
 useradd oracle
 echo 'qwer1234!@#$' | passwd --stdin oracle
 su - oracle
 cd ~
 mkdir -p ct-data/config
-mkdir WebRoot
 exit
 cd /usr/local
 tar -zxvf jdk-8u65-linux-x64.gz
@@ -93,21 +93,25 @@ cd /usr/local/nginx-1.9.5
 make && make install
 exit
 eeooff
-scp oracle@192.168.21.209:/home/oracle/ct-data/ct-data-3.1.0.release.jar root@${webaddr}:/home/oracle/ct-data/
-scp oracle@192.168.21.209:/home/oracle/ct-data/startup.sh root@${webaddr}:/home/oracle/ct-data/
-scp oracle@192.168.21.209:/home/oracle/ct-data/config/application-product.properties root@${webaddr}:/home/oracle/ct-data/config/
-scp oracle@192.168.21.209:/home/oracle/ct-data/17monipdb.dat root@${webaddr}:/home/oracle/ct-data/
+scp /root/ct-data/ct-data-3.1.0.RELEASE.jar root@${webaddr}:/home/oracle/ct-data/
+scp /root/ct-data/startup.sh root@${webaddr}:/home/oracle/ct-data/
+scp /root/ct-data/config/application-product.properties root@${webaddr}:/home/oracle/ct-data/config/
+scp /root/ct-data/17monipdb.dat root@${webaddr}:/home/oracle/ct-data/
+scp /root/WebRoot.zip root@${webaddr}:/home/oracle/
+scp /root/ct-data/nginx.conf root@${webaddr}:/usr/local/nginx/conf/
+chown -R oracle:oracle /home/oracle
 echo "--------------------------------------------------------------------------------------"
 
 echo "--------------------------------------------------------------------------------------"
-webaddr=
-hname=
+webaddr=192.168.20.103
+hname=fhc-admin
 echo "$webaddr admin start ..."
-scp /usr/local/jdk-8u65-linux-x64.gz root@${webaddr}:/usr/local/
-scp /usr/local/nginx-1.9.5.tar.gz root@${webaddr}:/usr/local/
+scp /root/jdk-8u65-linux-x64.gz root@${webaddr}:/usr/local/
+scp /root/nginx-1.9.5.tar.gz root@${webaddr}:/usr/local/
 ssh root@$webaddr << eeooff
 hostnamectl set-hostname $hname
-echo 'zxcv4321$#@!' | passwd --stdin root
+yum -y install gcc-c++
+yum -y install telnet
 useradd oracle
 echo 'qwer1234!@#$' | passwd --stdin oracle
 su - oracle
@@ -129,23 +133,26 @@ cd /usr/local/nginx-1.9.5
 make && make install
 exit
 eeooff
-scp oracle@192.168.21.212:/home/oracle/ct-data-admin/ct-data-admin-3.1.0.RELEASE.jar root@${webaddr}:/home/oracle/ct-data-admin/
-scp oracle@192.168.21.212:/home/oracle/ct-data-admin/startup.sh root@${webaddr}:/home/oracle/ct-data-admin/
-scp oracle@192.168.21.212:/home/oracle/ct-data-admin/config/application-product.properties root@${webaddr}:/home/oracle/ct-data-admin/config/
-scp oracle@192.168.21.212:/home/oracle/ct-data-admin/17monipdb.dat root@${webaddr}:/home/oracle/ct-data-admin/
-scp oracle@192.168.21.212:/home/oracle/tiso_feed_web/tiso_feed_web-3.0.0.release.jar root@${webaddr}:/home/oracle/tiso_feed_web/
-scp oracle@192.168.21.212:/home/oracle/tiso_feed_web/startup.sh root@${webaddr}:/home/oracle/tiso_feed_web/
-scp oracle@192.168.21.212:/home/oracle/tiso_feed_web/config/application-product.properties root@${webaddr}:/home/oracle/tiso_feed_web/config/
+scp /root/ct-data-admin/ct-data-admin-3.1.0.RELEASE.jar root@${webaddr}:/home/oracle/ct-data-admin/
+scp /root/ct-data-admin/startup.sh root@${webaddr}:/home/oracle/ct-data-admin/
+scp /root/ct-data-admin/config/application-product.properties root@${webaddr}:/home/oracle/ct-data-admin/config/
+scp /root/ct-data-admin/17monipdb.dat root@${webaddr}:/home/oracle/ct-data-admin/
+scp /root/tiso_feed_web/tiso_feed_web-3.0.0.RELEASE.jar root@${webaddr}:/home/oracle/tiso_feed_web/
+scp /root/tiso_feed_web/startup.sh root@${webaddr}:/home/oracle/tiso_feed_web/
+scp /root/tiso_feed_web/config/application-product.properties root@${webaddr}:/home/oracle/tiso_feed_web/config/
+scp /root/ct-data-admin/nginx.conf root@${webaddr}:/usr/local/nginx/conf/
+chown -R oracle:oracle /home/oracle
 echo "--------------------------------------------------------------------------------------"
 
 echo "--------------------------------------------------------------------------------------"
-webaddr=
-hname=
+webaddr=192.168.20.101
+hname=fhc-open
 echo "$webaddr open start ..."
-scp /usr/local/jdk-8u65-linux-x64.gz root@${webaddr}:/usr/local/
+scp /root/jdk-8u65-linux-x64.gz root@${webaddr}:/usr/local/
 ssh root@$webaddr << eeooff
 hostnamectl set-hostname $hname
-echo 'zxcv4321$#@!' | passwd --stdin root
+yum -y install gcc-c++
+yum -y install telnet
 useradd oracle
 echo 'qwer1234!@#$' | passwd --stdin oracle
 su - oracle
@@ -160,13 +167,14 @@ alternatives --install /usr/sbin/java java /usr/local/jdk1.8.0_65/bin/java 1
 alternatives --install /usr/sbin/jps jps /usr/local/jdk1.8.0_65/bin/jps 1
 exit
 eeooff
-scp oracle@192.168.21.216:/home/oracle/ct-data-open/ct-data-open-3.1.0.RELEASE.jar root@${webaddr}:/home/oracle/ct-data-open/
-scp oracle@192.168.21.216:/home/oracle/ct-data-open/startup.sh root@${webaddr}:/home/oracle/ct-data-open/
-scp oracle@192.168.21.216:/home/oracle/ct-data-open/config/application-product.properties root@${webaddr}:/home/oracle/ct-data-open/config/
-scp oracle@192.168.21.216:/home/oracle/ct-data-report/ct-data-report4-4.0.0.RELEASE.jar root@${webaddr}:/home/oracle/ct-data-report/
-scp oracle@192.168.21.216:/home/oracle/ct-data-report/startup.sh root@${webaddr}:/home/oracle/ct-data-report/
-scp oracle@192.168.21.216:/home/oracle/ct-data-report/config/application-product.properties root@${webaddr}:/home/oracle/ct-data-report/config/
-scp oracle@192.168.21.216:/home/oracle/ct-data-reportold/ct-data-report4.jar root@${webaddr}:/home/oracle/ct-data-reportold/
-scp oracle@192.168.21.216:/home/oracle/ct-data-reportold/startup.sh root@${webaddr}:/home/oracle/ct-data-reportold/
-scp oracle@192.168.21.216:/home/oracle/ct-data-reportold/config/application-product-old.properties root@${webaddr}:/home/oracle/ct-data-reportold/config/
+scp /root/ct-data-open/ct-data-open-3.1.0.RELEASE.jar root@${webaddr}:/home/oracle/ct-data-open/
+scp /root/ct-data-open/startup.sh root@${webaddr}:/home/oracle/ct-data-open/
+scp /root/ct-data-open/config/application-product.properties root@${webaddr}:/home/oracle/ct-data-open/config/
+scp /root/ct-data-report/ct-data-report4-4.0.0.RELEASE.jar root@${webaddr}:/home/oracle/ct-data-report/
+scp /root/ct-data-report/startup.sh root@${webaddr}:/home/oracle/ct-data-report/
+scp /root/ct-data-report/config/application-product.properties root@${webaddr}:/home/oracle/ct-data-report/config/
+scp /root/ct-data-reportold/ct-data-report4.jar root@${webaddr}:/home/oracle/ct-data-reportold/
+scp /root/ct-data-reportold/startup.sh root@${webaddr}:/home/oracle/ct-data-reportold/
+scp /root/ct-data-reportold/config/application-product-old.properties root@${webaddr}:/home/oracle/ct-data-reportold/config/
+chown -R oracle:oracle /home/oracle
 echo "--------------------------------------------------------------------------------------"
